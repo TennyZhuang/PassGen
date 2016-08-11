4feaf02931e3e72ceb0d52f50003e5bd06c7cc97
TennnyZhuang <zty0826@gmail.com>
2016-08-11 14:23:44 +0800

    convert pass to spec length

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,37 @@
 (() => {
+  String.lowers = Array.from('abcdefghijklmnopqrstuvwxyz');
+  String.uppers = String.lowers.map(c => c.toUpperCase());
+  String.numbers = _.range(10).map(i => i.toString());
+
+  Array.prototype.sum = function () {
+    return this.reduce((a, b) => a + b);
+  }
+
   new Vue({
     el: '#passgen',
     data: {
       keysentence: '',
-      salt: ''
+      salt: '',
+      len: 16
     },
     computed: {
+      charset() {
+        let s = [];
+        s = s.concat(String.lowers).concat(String.uppers).concat(String.numbers);
+        return s;
+      },
       password() {
-        return sha256(this.keysentence + this.salt);
+        const charset = this.charset;
+        const len = this.len || 16;
+        const pass64 = sha256(this.keysentence + this.salt);
+        const proportion = parseInt(64 / len);
+        const codes = Array.from(pass64).map(c => parseInt(c, 16)).slice(0, this.len * proportion);
+        const pass = _.chunk(codes, proportion)
+                      .map(a => (a.map((e, idx) => Math.pow(16, idx) * e).sum()))
+                      .map(v => charset[v % charset.length])
+                      .join('');
+
+        return pass;
       }
     }
   });
