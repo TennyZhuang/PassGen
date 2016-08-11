45162b11d88088d15e2ecd2f56f6e43f36e7f1bb
TennnyZhuang <zty0826@gmail.com>
2016-08-11 14:33:34 +0800

    support for options

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,12 +12,26 @@
     data: {
       keysentence: '',
       salt: '',
-      len: 16
+      len: 16,
+      lower: true,
+      upper: true,
+      number: true
     },
     computed: {
       charset() {
         let s = [];
-        s = s.concat(String.lowers).concat(String.uppers).concat(String.numbers);
+        if (this.lower) {
+          s = s.concat(String.lowers);
+        }
+
+        if (this.upper) {
+          s = s.concat(String.uppers);
+        }
+
+        if (this.number) {
+          s = s.concat(String.numbers);
+        }
+
         return s;
       },
       password() {
@@ -25,7 +39,7 @@
         const len = this.len || 16;
         const pass64 = sha256(this.keysentence + this.salt);
         const proportion = parseInt(64 / len);
-        const codes = Array.from(pass64).map(c => parseInt(c, 16)).slice(0, this.len * proportion);
+        const codes = Array.from(pass64).map(c => parseInt(c, 16)).slice(0, len * proportion);
         const pass = _.chunk(codes, proportion)
                       .map(a => (a.map((e, idx) => Math.pow(16, idx) * e).sum()))
                       .map(v => charset[v % charset.length])
