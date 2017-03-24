7298f51370e27aff19802d9321a4ea1b44668afe
TennnyZhuang <zty0826@gmail.com>
2017-03-24 22:36:42 +0800

    add debounce feature

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,20 @@
     return this.reduce((a, b) => a + b);
   }
 
+  const update_password = function () {
+    const charset = this.charset;
+    const len = this.len || 16;
+    const pass64 = sha256(this.keysentence + this.salt);
+    const proportion = parseInt(64 / len);
+    const codes = Array.from(pass64).map(c => parseInt(c, 16)).slice(0, len * proportion);
+    const pass = _.chunk(codes, proportion)
+                  .map(a => (a.map((e, idx) => Math.pow(16, idx) * e).sum()))
+                  .map(v => charset[v % charset.length])
+                  .join('');
+
+    this.password = pass;
+  };
+
   new Vue({
     el: '#passgen',
     data: {
@@ -15,7 +29,8 @@
       len: 16,
       lower: true,
       upper: true,
-      number: true
+      number: true,
+      password: '',
     },
     computed: {
       charset() {
@@ -34,19 +49,14 @@
 
         return s;
       },
-      password() {
-        const charset = this.charset;
-        const len = this.len || 16;
-        const pass64 = sha256(this.keysentence + this.salt);
-        const proportion = parseInt(64 / len);
-        const codes = Array.from(pass64).map(c => parseInt(c, 16)).slice(0, len * proportion);
-        const pass = _.chunk(codes, proportion)
-                      .map(a => (a.map((e, idx) => Math.pow(16, idx) * e).sum()))
-                      .map(v => charset[v % charset.length])
-                      .join('');
-
-        return pass;
-      }
+    },
+    watch: {
+      keysentence: update_password,
+      salt: update_password,
+      len: update_password,
+      lower: update_password,
+      upper: update_password,
+      number: update_password,
     }
   });
 })();
